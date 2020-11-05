3553bbad4eb4eb8e7fbff0493b232e58b37c5f65
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-11-05 10:02:22 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+const times = [
+    '08:00',
+    '08:50',
+    '09:40',
+    '10:30',
+    '10:45',
+    '11:35',
+    '12:25',
+    '13:15',
+    '14:00',
+    '14:45',
+    '14:50',
+    '15:35',
+    '16:20'
+];
+
+(function main() {
+    registerScheduleHandler();
+})();
+
+function registerScheduleHandler() {
+    times.forEach((time) => {
+
+    });
+}
+
+function playBell() {
+
+}
+
+function timeToCron(time) {
+    return cronTime
+}
